53a2b8db8d312ebbb820079b5e49741451d01245
tyler <tylercedge@gmail.com>
2019-02-23 12:35:04 -0500

    js file started

---
+++ assets/javascript/logic.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+
+    $('').on('click', function() {
+
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&"+
+    "api-key=J6xTRbHXv6zT3uu4tkhQYY8lm4npRUGL";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+
+    .then(function(response)) {
+        
+    }
+
+    });
+
+
+});
